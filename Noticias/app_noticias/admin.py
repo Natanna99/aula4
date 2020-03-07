99c0a3b8cdac371725e44a9069bb5b9a60791e61
Natanna Rocha Santos <Natannarocha@rede.ulbra.br>
2020-03-06 21:52:44 -0300

    novo

--- Noticias/app_noticias/admin.py
+++ Noticias/app_noticias/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Noticia
+from .models import Pessoa
+from .models import Tag
+from .models import Comentario
+from .models import Categoria
+from .models import Foto
 # Register your models here.
 
 @admin.register(Noticia)
+@admin.register(Pessoa)
+@admin.register(Tag)
+@admin.register(Comentario)
+@admin.register(Categoria)
+@admin.register(Foto)
+
 class NoticiaAdmin(admin.ModelAdmin):
     pass
--- Noticias/app_noticias/models.py
+++ Noticias/app_noticias/models.py
@@ -1,11 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Pessoa(models.Model):
+    usuario = models.OneToOneField(User, on_delete= models.CASCADE, verbose_name= 'Usuário')
+    nome = models.CharField('Nome', max_length= 128)
+    data_de_nascimento = models.DateField('Data de nascimento', blank = True)
+    telefone_celular = models.CharField('Telefone celular', max_length = 15, help_text = 'Numero do telefone celular no formato (99) 99999-9999', null = True, blank= True)
+    telefone_fixo = models.CharField('Telefone fixo', max_length = 15, help_text = 'Numero do telefone fixo no formato (99) 9999-9999', null = True, blank= True)
+    email = models.EmailField('E-mail', null = True, blank = True)
+
+    def __str__(self):
+        return self.nome
+    
+    pass
+
+class Tag(models.Model):
+    nome = models.CharField(max_length = 64)
+    slug = models.SlugField(max_length = 64)
+
+
+    def __str__(self):
+        return self.nome
+
+class Categoria(models.Model):
+    titulo = models.CharField("titulo", max_length = 64, blank = True, null = True)
+
+    def __str__(self):
+        return self.titulo
+
 class Noticia(models.Model):
     conteudo = models.TextField()
     data_publicacao = models.DateField('data publicação', max_length=128, blank=True, null=True)
     titulo = models.CharField('titulo', max_length=128, blank=True, null=True)
-    auto = models.CharField('autor', max_length=128, blank=True, null=True)
+    autor = models.ForeignKey(Pessoa, on_delete=models.CASCADE, blank=True, null=True)
+    tags = models.ManyToManyField(Tag)
+    categoria =  models.ForeignKey(Categoria, on_delete = models.CASCADE, blank = True, null = True)
+    foto = models.ForeignKey(Foto, on_delete = models.CASCADE, blank = True, null = True)
     
     def __str__(self):
         return self.titulo
@@ -13,5 +44,23 @@ class Noticia(models.Model):
     class Meta:
         verbose_name = "Notícia"
         verbose_name_plural = "Notícias"
+
+class Comentario(models.Model):
+    comentarios = models.TextField()
+    pessoas = models.ForeignKey(Pessoa, on_delete=models.CASCADE, blank=True, null=True)
+    data_hora = models.DateTimeField('data e hora do comentario', blank = True)
+    noticia = models.ForeignKey(Noticia, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.comentarios
+
+class Foto(models.Model):
+    titulo = models.CharField('Titulo', max_length = 64 , blank = True, null = True)
+    url = models.SlugField(max_length = 64)
+
+
+
     
+
+
     
